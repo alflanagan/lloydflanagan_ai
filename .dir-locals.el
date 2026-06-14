cd3a15f359dde1963d7f31c77b6b00e064c2f97b
A. Lloyd Flanagan (he/him) <lloyd.flanagan@proton.me>
2026-06-13 21:27:17 -0400

    Fix project-directory value, use typescript-ts-base-mode, enable editorconfig.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,14 @@
-((typescript-mode
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((mode . editorconfig)))
+ (python-base-mode
+  . ((flycheck-python-mypy-python-executable . "./.venv/bin/mypy")))
+ (typescript-ts-base-mode
   .
   ((eval .
-         (let ((project-directory
-                (car (dir-locals-find-file default-directory))))
+         (let (project-directory
+               (dir-locals-find-file default-directory))
            (setq lsp-clients-typescript-server-args
                  `("--tsserver-path" ,(concat
                      project-directory ".yarn/sdks/typescript/bin/tsserver")
